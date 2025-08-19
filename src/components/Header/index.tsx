259b4ca1bc2b98c1d0165baab5d6181cd3116140
leotaborda <leo.taboorda@gmail.com>
2025-08-19 15:47:37 -0300

    feat: update page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import HeaderList from './components/HeaderList'
+import HeaderListItem from './components/HeaderListItem'
+import Logo from '../Logo'
+import HeaderLinks from './components/HeaderLinks'
+import HeaderFormFilters from './components/HeaderFormFilters'
+import HeaderActions from './components/HeaderActions'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <HeaderList>
+        <HeaderListItem>
+          <Logo src='/icons/Logo.svg'/>
+        </HeaderListItem>
+        <HeaderListItem>
+          <HeaderLinks/>
+        </HeaderListItem>
+        <HeaderListItem>
+          <HeaderFormFilters/>
+        </HeaderListItem>
+        <HeaderListItem>
+          <HeaderActions/>
+        </HeaderListItem>
+      </HeaderList>
+    </header>
   )
 }
 
