57cdcef1d2b1b207286f70408927d105356b15df
leotaborda <leo.taboorda@gmail.com>
2025-08-20 14:38:41 -0300

    feat: add full new year and styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import FooterLogo from "./components/FooterLogo";
 import FooterFuncionamento from "./components/FooterFuncionamento";
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 import FooterInstucional from "./components/FooterInstucional";
 import FooterRedes from "./components/FooterSiganos";
 
 const Footer = () => {
   return (
-    <footer className={styles.rodape}>
-      <div className={styles.rodape_informacoes}>
-        <FooterLogo/>
-        <FooterFuncionamento/>
-        <FooterInstucional/>
-        <FooterRedes />
+    <>
+      <footer className={styles.rodape}>
+        <div className={styles.rodape_informacoes}>
+          <FooterLogo />
+          <FooterFuncionamento />
+          <FooterInstucional />
+          <FooterRedes />
+        </div>
+      </footer>
+      <div className={styles.desenvolvimento}>
+        <p><span>© {new Date().getFullYear()}</span> - Desenvolvido por Leonardo Taborda de Oliveira com Alura! Projeto fictício sem fins comercias.</p>
       </div>
-    </footer>
+    </>
   );
 };
 
