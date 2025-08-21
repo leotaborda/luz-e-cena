9bc8168f98ba0777fdb4f0ccd7b68b4a4ff5d4f7
leotaborda <leo.taboorda@gmail.com>
2025-08-21 15:31:46 -0300

    feat: add link in buttons footer

--- src/components/Footer/components/FooterSiganos.tsx
+++ src/components/Footer/components/FooterSiganos.tsx
@@ -1,14 +1,14 @@
 import styles from "../Footer.module.css";
-import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa6";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa6";
 
 const FooterRedes = () => {
   return (
     <div className={styles.redes}>
       <h4 className={styles.titulo}>Nossas Redes Sociais</h4>
       <div className={styles.icones}>
-        <FaGithub className={styles.icone} href=""/>
-        <FaInstagram className={styles.icone} href=""/>
-        <FaLinkedin className={styles.icone}  href=""/>
+        <FaGithub className={styles.icone} href="https://github.com/leotaborda/"/>
+        <FaInstagram className={styles.icone} href="https://www.instagram.com/_.taborda/"/>
+        <FaLinkedin className={styles.icone}  href="https://www.linkedin.com/in/leotaborda/"/>
       </div>
     </div>
   );
